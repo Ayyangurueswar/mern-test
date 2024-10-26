d7b66417005fa9d4ed7c163601d0afa3a31d9c96
Ayyangurueswar <ayyan16.gbps@gmail.com>
2024-10-26 23:37:06 +0530

    Allow PATCH

--- leaderboard/api/index.js
+++ leaderboard/api/index.js
@@ -13,7 +13,7 @@ await connectDb();
 
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.sendStatus(200);
 });
