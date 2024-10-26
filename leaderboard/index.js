752607d38a53358568668efc9291db83b118e3dd
Ayyangurueswar <ayyan16.gbps@gmail.com>
2024-10-26 22:25:46 +0530

    allow header 2

--- leaderboard/index.js
+++ leaderboard/index.js
@@ -19,10 +19,12 @@ app.use(
     allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
-app.options('*', cors());
-app.options('/*', (req, res) => {
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.sendStatus(200);
-})
+});
 
 //middle wares
 app.use(express.json());
